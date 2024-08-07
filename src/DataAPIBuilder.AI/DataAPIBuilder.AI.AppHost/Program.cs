ba7d9df765111927ab5a410da8fac2e28814af0c
Tommaso Stocchi <tstocchi@microsoft.com>
2024-08-07 13:58:25 +0200

    removes old connectionstrings

--- src/DataAPIBuilder.AI/DataAPIBuilder.AI.AppHost/Program.cs
+++ src/DataAPIBuilder.AI/DataAPIBuilder.AI.AppHost/Program.cs
@@ -1,8 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sql = builder.AddConnectionString("sql");
+
 var dabService = builder.AddContainer("dab", "mcr.microsoft.com/azure-databases/data-api-builder")
     .WithHttpEndpoint(targetPort: 5000, name: "http")
     .WithBindMount(@"D:\src\distributed-dab-sk\aw-data-api-builder\dab-config.json", "/App/dab-config.json", true)
+    .WithReference(sql)
     .WithOtlpExporter()
     .PublishAsContainer();
 var dabServiceEndpoint = dabService.GetEndpoint("http");
